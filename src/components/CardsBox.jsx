fa417371101aa13a23ad52f2bcff31dc119b9054
miguel mateo <mmateo95@hotmail.com>
2023-03-19 18:34:23 -0400

    Custom hook added, to make cards area dragable

--- src/components/CardsBox.jsx
+++ src/components/CardsBox.jsx
@@ -1,13 +1,12 @@
 import useDragger from '../hooks/useDragger';
 
 const CardsBox = () => {
-        
+    // custom hook, to make element dragable
     useDragger('cards-box')
 
     return (
         <section className='drag-area'>
             <div id='cards-box'>
-
              </div>
         </section>
         )
