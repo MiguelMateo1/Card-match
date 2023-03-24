1c9b12d9b5fd9fc8198a173b778dc55282b9fa56
miguel mateo <mmateo95@hotmail.com>
2023-03-24 16:11:09 -0400

    intro card added,, displays in card container

---
+++ src/components/IntroCard.jsx
@@ -0,0 +1,16 @@
+
+const IntroCard = ({}) => {
+
+    return (
+        <>
+        <section className="intro-img-container">
+            <aside className="intro-img">
+                <div className="front"></div>
+                <div className="back"></div>
+            </aside>
+        </section>
+        </>
+        )
+}
+
+export default IntroCard
