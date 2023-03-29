e00d9471d00a766dc61851fbc68e482d7efa3dfd
miguel mateo <mmateo95@hotmail.com>
2023-03-28 23:14:58 -0400

    added select level text

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,6 +6,7 @@ const Menu = ({start,end,levelSelect,active}) => {
         {active && <button className="btn end-btn" onClick={end}>End</button>}
         {!active && <button className="btn" onClick={start}>Start</button>}
         {!active && <button className="btn" onClick={levelSelect}>Level</button>}
+        <h5 className="level-selected-text">Selected level: 1</h5>
         </div>
         )
 }
