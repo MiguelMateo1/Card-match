0726350fc880f446bdbe25d7acdd546388369be5
miguel mateo <mmateo95@hotmail.com>
2023-04-01 10:33:30 -0400

    handleClick and handleCardsClick functions added

--- src/components/GetCards.jsx
+++ src/components/GetCards.jsx
@@ -2,18 +2,41 @@ import { levelOne } from "../CardImages/card-images"
 
 const GetCards = ({numOfCards}) => {
 
-    function hangleCardMatch (selectedCard) {
-        selectedCard.classList.remove('active');
+    function handleCardsClick (selectedCard) {
+        // remove active class
+        selectedCard.forEach((c) => c.classList.remove('active'));
+        // resets selected cards
+        selectedCards = [];
+        backOfcard = [];
     }
+
+    let selectedCards = [];
+    let backOfcard = [];
+    let matchedCards = [];
 
     function handleClick (e) {
-        const backOfcard = e.currentTarget.parentElement;
+        // backOfcard.push(e.target.parentElement);
+        console.log(selectedCards)
+        console.log(backOfcard)
         
-        backOfcard.classList.add('active');
-        setTimeout(hangleCardMatch, 2000, backOfcard)
+        // if card click add active class and push to selectedCards var
+        if (e.target.classList.contains('front')){
+                backOfcard.push(e.target.parentElement);
+                e.target.parentElement.classList.add('active');
+                selectedCards.push(e.target.parentElement.getAttribute('value'))
+                console.log(selectedCards)
+            }  
+         // TODO if Cards match put in a var and add a match class (or and in handleCardsClick func)
+
+
+        // once clicks on secuond card runs handleCardsClick func
+        if (selectedCards.length > 1 ) {
+                setTimeout(handleCardsClick, 2000, backOfcard)
+            }
     }
+    
 
-
+    // get and display cards functions below
     function getRandomOrder(level) {
         let holdRamdonNum = []
         let keys;
@@ -39,12 +62,12 @@ const GetCards = ({numOfCards}) => {
         shuffleCards(setOfCard)
         const dispaly = setOfCard.map((card) => {
             return (
-                <aside key={card.key}>
-                        <div className="front" onClick={handleClick}></div>
+                <aside key={card.key} value={card.key}>
+                        <div className="front"></div>
                         <div className="back" style={{backgroundImage: `url(${card.img})`}}></div>
                     </aside>
-            )
-        })
+                )
+            })
         return dispaly
     }
     // shuffle cards func
@@ -55,8 +78,10 @@ const GetCards = ({numOfCards}) => {
         }
     }
 
+    // TODO set up use effect/next level btn to run when game is over and display next level cards?
+
     return (
-        <div className="card-display">
+        <div className="card-display" onClick={handleClick}>
             {displayCards(getRandomOrder(levelOne))}
         </div>
         )
