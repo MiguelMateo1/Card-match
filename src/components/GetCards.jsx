774f5d2704b3bfda771a5b13c01663304e613510
Miguel Mateo <mmateo95@hotmail.com>
2023-04-02 22:20:51 -0400

    moved functions to getCardsFucn.jsx and impoted them(to make file look cleaner)

--- src/components/GetCards.jsx
+++ src/components/GetCards.jsx
@@ -1,4 +1,4 @@
-import { levelOne } from "../CardImages/card-images"
+import { cards, getRandomOrder, displayCards } from "../getCardsFunc";
 
 const GetCards = ({numOfCards}) => {
 
@@ -33,55 +33,10 @@ const GetCards = ({numOfCards}) => {
                 setTimeout(handleCardsClick, 2000, backOfcard)
             }
     }
-    
-
-    // get and display cards functions below
-    function getRandomOrder(level) {
-        let holdRamdonNum = []
-        let keys;
-        let randomOrder = []
-        // for-loop to have cards ramdomly ordered in the randomOrder array
-        for (let i=0;i<level.length;i++){
-            keys = level[Math.floor(Math.random() * level.length)]
-            
-            // checks if key is alrady added/ to not have duplicates
-            while (holdRamdonNum.includes(keys.key)) {
-                keys = level[Math.floor(Math.random() * level.length)]
-            }
-            holdRamdonNum.push(keys.key)
-            randomOrder.push(keys)
-        };
-        // doubles the array
-        const cardSet = randomOrder.concat(randomOrder);
-        return cardSet;
-    }
-
-    // displays cards
-    function displayCards (setOfCard) {
-        shuffleCards(setOfCard)
-        const dispaly = setOfCard.map((card) => {
-            return (
-                <aside key={card.key} value={card.key}>
-                        <div className="front"></div>
-                        <div className="back" style={{backgroundImage: `url(${card.img})`}}></div>
-                    </aside>
-                )
-            })
-        return dispaly
-    }
-    // shuffle cards func
-    function shuffleCards(cards) {
-        for (let i = cards.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [cards[i], cards[j]] = [cards[j], cards[i]];
-        }
-    }
-
-    // TODO set up use effect/next level btn to run when game is over and display next level cards?
 
     return (
         <div className="card-display" onClick={handleClick}>
-            {displayCards(getRandomOrder(levelOne))}
+            {displayCards(getRandomOrder(cards(8)))}
         </div>
         )
 }
