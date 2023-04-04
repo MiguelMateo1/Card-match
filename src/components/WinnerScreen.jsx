215bbbcfd22d21749aeb92267e1efbe418a2266f
Miguel Mateo <mmateo95@hotmail.com>
2023-04-03 23:46:03 -0400

    recfactor

--- src/components/WinnerScreen.jsx
+++ src/components/WinnerScreen.jsx
@@ -1,17 +1,9 @@
 
 const WinnerScreen = ({level}) => {
 
-    const numOfCards = [{level: 1,cards: 16},{level: 2,cards: 20},{level: 3,cards: 24},
-        {level: 4,cards: 30},{level: 5,cards: 36}]
-
-        const nextLevel = numOfCards[{level}];
-
-        
         const endBtnHandle = () => {
             setActive(!true)
         }
-
-
 
     return (
         <div className="winner-screen">
