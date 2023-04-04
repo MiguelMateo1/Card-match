8eb5c89e40a1c45b348ae87fa8a1574943696099
Miguel Mateo <mmateo95@hotmail.com>
2023-04-03 23:35:14 -0400

    refactor

--- src/components/GetCards.jsx
+++ src/components/GetCards.jsx
@@ -5,13 +5,6 @@ import GameStats from "./GameStats";
 
 const GetCards = ({numOfCards}) => {
     const [win, setWin] = useState(false)
-    const [m, setM] = useState(0)
-    console.log('mmmm',m)
-    console.log('page run')
-
-    // useEffect (() => {
-    //     console.log('its a win')
-    // },[win]);
 
     function handleCardsClick (selectedCard) {
         // remove active class
@@ -24,13 +17,10 @@ const GetCards = ({numOfCards}) => {
     let selectedCards = [];
     let backOfcard = [];
     let moves = 0;
-    console.log('movesss', moves)
     let matchedCards = 0;
-
 
     let lastSelectCard = [];
     let lastSelectNode = null;
-    console.log('last seleed', lastSelectCard)
 
     function handleClick (e) {
         
@@ -38,7 +28,6 @@ const GetCards = ({numOfCards}) => {
         if (e.target.classList.contains('front')){
             console.log(e.target.parentElement.childNodes[1])        
                 moves += 1;
-                console.log('moves', moves)
                 backOfcard.push(e.target.parentElement);
                 e.target.parentElement.classList.add('active');
                 selectedCards.push(e.target.parentElement.getAttribute('value'))
@@ -49,14 +38,12 @@ const GetCards = ({numOfCards}) => {
             
          // TODO if Cards match put in a var and add a match class (or and in handleCardsClick func)
         if (selectedCards[0] == selectedCards[1]){
-            console.log('seleted if rannn')
+            console.log('seleted if ran')
             matchedCards += 1
-            console.log('mached cards', matchedCards)
             lastSelectNode.childNodes[1].classList.add('matched')
             e.target.parentElement.childNodes[1].classList.add('matched')
             if (matchedCards == numOfCards.cards / 2) {
                 setWin(true)
-                console.log(win)
             }
             lastSelectNode = null;
             selectedCards = [];
