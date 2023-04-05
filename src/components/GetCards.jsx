1988ecd24f7927b6492f23c4a63535208dc5bfcd
Miguel Mateo <mmateo95@hotmail.com>
2023-04-04 20:59:53 -0400

    added useEffect for showWonScreen, change variable to useStates and removed moves var

--- src/components/GetCards.jsx
+++ src/components/GetCards.jsx
@@ -4,9 +4,12 @@ import DisplayCards from "./DisplayCards";
 import GameStats from "./GameStats";
 
 const GetCards = ({numOfCards}) => {
-    const [win, setWin] = useState(false)
+    const [win, setWin] = useState(false);
+    const [visible, setVisible] = useState(true);
+    const [matchedCards, setMatchedCards] = useState(0);
 
-    function handleCardsClick (selectedCard) {
+    function handleClickOnSecoundCard (selectedCard) {
+        setVisible((prev) => !prev)
         // remove active class
         selectedCard.forEach((c) => c.classList.remove('active'));
         // resets selected cards
@@ -16,35 +19,40 @@ const GetCards = ({numOfCards}) => {
 
     let selectedCards = [];
     let backOfcard = [];
-    let moves = 0;
-    let matchedCards = 0;
 
-    let lastSelectCard = [];
+    function showWonScreen () {
+        if (matchedCards == numOfCards.cards / 2) {
+            setWin(true)
+        }
+        return;
+    }
+
+    useEffect(() => {
+        showWonScreen()
+    },[matchedCards]);
+
     let lastSelectNode = null;
 
+    // handle card click
     function handleClick (e) {
-        
         // if card click add active class and push to selectedCards var
         if (e.target.classList.contains('front')){
             console.log(e.target.parentElement.childNodes[1])        
-                moves += 1;
                 backOfcard.push(e.target.parentElement);
                 e.target.parentElement.classList.add('active');
-                selectedCards.push(e.target.parentElement.getAttribute('value'))
+                selectedCards.push(e.target.parentElement.getAttribute('value'));
                 console.log('selected cards', selectedCards)
                 if (lastSelectNode == null) {
-                    lastSelectNode = e.target.parentElement
+                    lastSelectNode = e.target.parentElement;
                 }
             
-         // TODO if Cards match put in a var and add a match class (or and in handleCardsClick func)
+         // TODO if Cards match put in a var and add a match class (or and in handleClickOnSecoundCard func)
         if (selectedCards[0] == selectedCards[1]){
             console.log('seleted if ran')
-            matchedCards += 1
+            setMatchedCards(matchedCards + 1)
             lastSelectNode.childNodes[1].classList.add('matched')
             e.target.parentElement.childNodes[1].classList.add('matched')
-            if (matchedCards == numOfCards.cards / 2) {
-                setWin(true)
-            }
+            
             lastSelectNode = null;
             selectedCards = [];
             backOfcard = [];
@@ -54,17 +62,18 @@ const GetCards = ({numOfCards}) => {
         // once clicks on secound card runs handleCardsClick func
         if (selectedCards.length > 1 ) {
             console.log('clear rann')
-                 lastSelectNode = null;
-                setTimeout(handleCardsClick, 2000, backOfcard)
+            setVisible((prev) => !prev)
+                lastSelectNode = null;
+                setTimeout(handleClickOnSecoundCard, 2000, backOfcard)
             }
         }
     }
 
     return (
-        <div className="card-display" onClick={handleClick}>
+        <div className="card-display" onClick={visible && handleClick}>
             {win && <WinnerScreen level={numOfCards.level}/>}
-            {!win && <DisplayCards numOfCards={numOfCards}/>}
-            {!win && <GameStats active={true} matched={matchedCards} moves={moves}/>} 
+            {!win && <DisplayCards numOfCards={numOfCards} />}
+            {!win && <GameStats active={true} matched={matchedCards}/>} 
         </div>
     )
 }
