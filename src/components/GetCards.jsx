cb693c8a5843ab4b4c43aee219950dc899fd6761
Miguel Mateo <mmateo95@hotmail.com>
2023-04-06 12:25:21 -0400

    cardMatchedSound function added and refactor code/remove console logs

--- src/components/GetCards.jsx
+++ src/components/GetCards.jsx
@@ -8,7 +8,7 @@ const GetCards = ({numOfCards}) => {
     const [visible, setVisible] = useState(true);
     const [matchedCards, setMatchedCards] = useState(0);
 
-    function handleClickOnSecoundCard (selectedCard) {
+    function handleClickOnSecondCard (selectedCard) {
         setVisible((prev) => !prev)
         // remove active class
         selectedCard.forEach((c) => c.classList.remove('active'));
@@ -17,8 +17,16 @@ const GetCards = ({numOfCards}) => {
         backOfcard = [];
     }
 
+    function cardMatchedSound () {
+        let audio = new Audio('../public/music/mached-sound.mp3')
+        audio.volume = 0.04
+        audio.play()
+        setMatchedCards(matchedCards + 1)
+    }
+
     let selectedCards = [];
     let backOfcard = [];
+    let lastSelectNode = null;
 
     function showWonScreen () {
         if (matchedCards == numOfCards.cards / 2) {
@@ -30,25 +38,20 @@ const GetCards = ({numOfCards}) => {
         showWonScreen()
     },[matchedCards]);
 
-    let lastSelectNode = null;
-
     // handle card click
     function handleClick (e) {
         // if card click add active class and push to selectedCards var
-        if (e.target.classList.contains('front')){
-            console.log(e.target.parentElement.childNodes[1])        
+        if (e.target.classList.contains('front')){    
                 backOfcard.push(e.target.parentElement);
                 e.target.parentElement.classList.add('active');
                 selectedCards.push(e.target.parentElement.getAttribute('value'));
-                console.log('selected cards', selectedCards)
                 if (lastSelectNode == null) {
                     lastSelectNode = e.target.parentElement;
                 }
             
-         // TODO if Cards match put in a var and add a match class (or and in handleClickOnSecoundCard func)
+         // TODO if Cards match put in a var and add a match class (or and in handleClickOnSecondCard func)
         if (selectedCards[0] == selectedCards[1]){
-            console.log('seleted if ran')
-            setMatchedCards(matchedCards + 1)
+            cardMatchedSound()
             lastSelectNode.childNodes[1].classList.add('matched')
             e.target.parentElement.childNodes[1].classList.add('matched')
             
@@ -60,10 +63,9 @@ const GetCards = ({numOfCards}) => {
 
         // once clicks on secound card runs handleCardsClick func
         if (selectedCards.length > 1 ) {
-            console.log('clear rann')
             setVisible((prev) => !prev)
                 lastSelectNode = null;
-                setTimeout(handleClickOnSecoundCard, 2000, backOfcard)
+                setTimeout(handleClickOnSecondCard, 2000, backOfcard)
             }
         }
     }
