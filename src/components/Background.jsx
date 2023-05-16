2283957d4719bb423bf4a59f92d37acadef90360
miguel mateo <mmateo95@hotmail.com>
2023-05-16 19:25:10 -0400

    changed font layer Y parallax #

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -17,7 +17,7 @@ const Backgound = () => {
             <div className='container'>
                 <div ref={sceneRef} id='scene'>
                     <div className='layer moon' data-depth-y="0.1" data-depth-x="0.15"><img src={moon}></img></div>
-                    <div className='layer front' data-depth-y="0.03" data-depth-x="0.2"><img src={front}></img></div>
+                    <div className='layer front' data-depth-y="0.003" data-depth-x="0.2"><img src={front}></img></div>
                     <div className='layer mid' data-depth-y="0.13" data-depth-x="0.35"><img src={mid}></img></div>
                     <div className='layer clif' data-depth-y="-0.08" data-depth-x="0.1"><img src={clif}></img></div>
                 </div>
